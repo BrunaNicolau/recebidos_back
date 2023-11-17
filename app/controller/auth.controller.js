6b94efc4423ceb5512a37ebd613afda966008ab2
Bruna Nicolau <brunanicolau456@gmail.com>
2023-11-17 16:06:42 -0300

    integracao auth

---
+++ app/controller/auth.controller.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const authDB = require("../repository/comuns.repository");
+const LocalStrategy = require("passport-local").Strategy;
+const passport = require("passport");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const users = await authDB.checkUser(username, password);
+
+      if (!users.length) {
+        return done(null, false, { message: "Invalid credentials" });
+      }
+
+      const user = users[0];
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+// Passport Routes
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    return res.json({ success: true, user });
+  })(req, res, next);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const session = require("express-session");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/recebidos", require("./app/controller/comuns.controller"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 60 * 1000 },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/recebidos", require("./app/controller/auth.controller"));
 app.use("/recebidos/office", require("./app/controller/office.controller"));
 app.use("/recebidos/receipt", require("./app/controller/receipt.controller"));
 app.use("/recebidos/donor", require("./app/controller/donor.controller"));
